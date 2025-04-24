131a29e763e376a2f60b09a6b03db157d3a4e6e1
Gunnar Liden <gunnar.liden@jazzliveaarau.ch>
2025-04-24 17:34:34 +0200

    Local storage for new season input data

---
+++ Scripts/NewSeasonStorage.js
@@ -0,0 +1,256 @@
+// File: NewSeasonStorage.js
+// Date: 2025-04-24
+// Author: Gunnar Lidén
+
+// Class handling the storage of new season data in the computer
+//
+// Please note that session data not yet is used
+
+class NewSeasonStorage
+{
+    ///////////////////////////////////////////////////////////////////////////
+    /////// Start Set Functions ///////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Sets the new season data as windows local storage. 
+    // i_new_season_data: An instance of the class NewSeasonData
+    static setLocal(i_new_season_data)
+    {
+        localStorage.setItem(NewSeasonStorage.keyMainDir(), i_new_season_data.getMainDir());
+
+        localStorage.setItem(NewSeasonStorage.keyResultDir(), i_new_season_data.getResultDir());
+
+    } // setLocal
+
+    // Sets the layout data as windows session storage. 
+    // i_new_season_data: An instance of the class NewSeasonData
+    static setSession(i_new_season_data)
+    {
+        sessionStorage.setItem(NewSeasonStorage.keyMainDir(), i_new_season_data.getMainDir());
+
+        sessionStorage.setItem(NewSeasonStorage.keyResultDir(), i_new_season_data.getResultDir());
+
+    } // setSession
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// End Set Functions /////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// Start Get Functions ///////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Returns a reservation new season data object NewSeasonData with data from the 
+    // windows local storage.
+    static getLocal()
+    {
+        var ret_new_season_data = new NewSeasonData();
+
+        var main_dir = localStorage.getItem(NewSeasonStorage.keyMainDir());
+
+        var result_dir = localStorage.getItem(NewSeasonStorage.keyResultDir());
+
+        if (main_dir != null && main_dir.length > 5 && main_dir != NewSeasonStorage.undefinedStringValue())
+        {
+            ret_new_season_data.setMainDir(main_dir);
+        }
+
+        if (result_dir != null && result_dir.length > 5 && result_dir != NewSeasonStorage.undefinedStringValue())
+        {
+            ret_new_season_data.setResultDir(result_dir);
+        }        
+
+        return ret_new_season_data;
+
+    } // getLocal
+
+    // Returns a reservation new season data object NewSeasonData with data from the 
+    // windows session storage. 
+    static getSession()
+    {
+        var ret_new_season_data = new NewSeasonData();
+
+        var main_dir = sessionStorage.getItem(NewSeasonStorage.keyMainDir());
+
+        var result_dir = sessionStorage.getItem(NewSeasonStorage.keyResultDir());
+
+        ret_new_season_data.setMainDir(main_dir);
+
+        ret_new_season_data.setResultDir(result_dir);
+
+        return ret_new_season_data;
+
+    } // getSession
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// End Get Functions /////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// Start Init Functions //////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Initialization of not yet set storage variables
+    static initLocal()
+    {
+        var main_dir = localStorage.getItem(NewSeasonStorage.keyMainDir());
+
+        var result_dir = localStorage.getItem(NewSeasonStorage.keyResultDir());
+
+        if (null == main_dir)
+        {
+            main_dir = NewSeasonStorage.undefinedStringValue();
+
+            localStorage.setItem(NewSeasonStorage.keyMainDir(), main_dir);
+        }
+
+        if (null == result_dir)
+        {
+            result_dir = NewSeasonStorage.undefinedStringValue();
+
+            localStorage.setItem(NewSeasonStorage.keyResultDir(), result_dir);
+        }
+
+    } // initLocal
+
+    // Initialization of not yet set session variables
+    static initSession()
+    {
+        var main_dir = sessionStorage.getItem(NewSeasonStorage.keyMainDir());
+
+        var result_dir = sessionStorage.getItem(NewSeasonStorage.keyResultDir());
+
+        if (null == main_dir)
+        {
+            main_dir = NewSeasonStorage.undefinedStringValue();
+
+            sessionStorage.setItem(NewSeasonStorage.keyMainDir(), main_dir);
+        }
+
+        if (null == result_dir)
+        {
+            result_dir = NewSeasonStorage.undefinedStringValue();
+
+            sessionStorage.setItem(NewSeasonStorage.keyResultDir(), result_dir);
+        }
+
+    } // initSession
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// End Init Functions ////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// Start Get Storage Keys ////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Key organisation directory name
+    static keyMainDir()
+    {
+        return 'new_season_main_dir_name_str';
+        
+    } // keyMainDir
+
+    // Key result directory name
+    static keyResultDir()
+    {
+        return 'new_season_result_dir_name_str';
+        
+    } // keyResultDir
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// End Get Storage Keys //////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// Start Get Undefined Value /////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    // Returns a string (flag) telling that the variable not yet was set
+    static undefinedStringValue()
+    {
+        return '';
+
+    } // undefinedStringValue
+
+    ///////////////////////////////////////////////////////////////////////////
+    /////// End Get Undefined Value ///////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+} // NewSeasonStorage
+
+// Class that hold data for the creation of new season files
+class NewSeasonData
+{
+    constructor()
+    {
+        // Name of the server main directory name
+        // Only Reservation (release) and ReservationLayout (test) are allowed
+        this.m_main_dir_name = "ReservationLayout";
+
+        // Name of the server result directory name
+        this.m_result_directory_name = "Spagi_76_Chairs_V_2"; //QQ Temporary
+
+        // Flag telling if data is OK
+        this.m_data_is_valid = true;
+
+    } // constructor
+
+    // Returns the name of the new server season main directory name
+    getMainDir()
+    {
+        return this.m_main_dir_name;
+
+    } // getMainDir
+
+    // Sets the name of the new server season main directory name
+    setMainDir(i_main_dir_name)
+    {
+        return this.m_main_dir_name = i_main_dir_name;
+
+    } // setMainDir
+
+    // Returns the name of the server result directory name
+    getResultDir()
+    {
+        return this.m_result_directory_name;
+
+    } // getResultDir
+
+    // Sets the name of the server result directory name
+    setResultDir(i_result_directory_name)
+    {
+        return this.m_result_directory_name = i_result_directory_name;
+
+    } // setResultDir
+
+    // Checks the data 
+    checkData()
+    {
+        this.m_data_is_valid = true;
+        
+        if (this.m_main_dir_name != 'Reservation' && this.m_main_dir_name != 'ReservationLayout')
+        {
+            alert("Gültige Ordner-Namen sind Reservation und ReservationLayout. Nicht " + this.m_main_dir_name);
+
+            this.m_data_is_valid = false;
+        }
+
+        if (this.m_result_directory_name.trim().length < 5)
+        {
+            alert(" Name des Konzerztsaal-Ordners zu kurz.  Nicht " + this.m_result_directory_name);
+
+            this.m_data_is_valid = false;           
+        }
+
+
+    } // checkData
+
+    // Returns true if data is valid. Note checkData must be executed!
+    dataIsValid()
+    {
+        return this.m_data_is_valid;
+
+    } // dataIsValid
+
+} // NewSeasonData
--- Scripts/ReservationNewSeason.js
+++ Scripts/ReservationNewSeason.js
@@ -11,6 +11,12 @@
 ///////////////////////// Start Global Parameters /////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+// Main directory 
+var g_layout_main_dir_text_box = null;
+
+// Result directory where the generated HTML files and other files shall be stored
+var g_layout_server_dir_text_box = null;
+
 // Button for the generation of new season XML files
 var g_xml_create_new_button = null;
 
@@ -22,16 +28,75 @@ var g_xml_create_new_button = null;
 ///////////////////////// Start Main Functions ////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+// Initialization
+// 1. If local storage not is set (after delete browser cache) set empty strings
+//    Call of NewSeasonStorage.initLocal
+// 2. Create the controls for this application
+//    Call of createReservationNewSeasonControls
+// 3. Set the controls with data from local storage
+//    Call of NewSeasonStorage.getLocal and setNewSeasonControls
 function initReservationNewSeason()
 {
     debugReservationNewSeason('initReservationNewSeason Enter');
 
+    NewSeasonStorage.initLocal();
+
     createReservationNewSeasonControls();
+
+    new_season_data = NewSeasonStorage.getLocal();
+
+    setNewSeasonControls(new_season_data);
 
 } // initReservationNewSeason
 
+// Set the controls
+function setNewSeasonControls(i_new_season_data)
+{
+    g_layout_main_dir_text_box.setValue(i_new_season_data.getMainDir());  
+
+    g_layout_server_dir_text_box.setValue(i_new_season_data.getResultDir()); 
+
+} // setNewSeasonControls
+
+// Create the event program XML file
+function execCreateEventProgramXmlFile()
+{
+    var input_data = getNewSeasonDataInput();
+
+    if (!input_data.dataIsValid())
+    {
+        return;
+    }
+
+    var season_case = 'current';
+
+    var main_dir = input_data.getMainDir(); //Reservation or ReservationLayout
+   
+    var result_dir = input_data.getResultDir();
+
+    var sub_xml_dir = 'SaisonXML';
+
+    var xml_filename = 'EventProgram.xml';
+
+    var callback_fctn = callbackEventProgramCreated;
+
+    var create_event_xml = new  SeasonToEventProgramXml(season_case, main_dir, result_dir, sub_xml_dir, xml_filename, callback_fctn);
+
+} // execCreateEventProgramXmlFile
+
+// 
+function callbackEventProgramCreated()
+{
+    setNewSeasonLocalStorageData();
+
+    alert("Event Program XML file created");
+
+    //TODO createNewXmlEventFiles();
+
+} // callbackEventProgramCreated
+
 ///////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////// Start Create Controls ///////////////////////////////////////////
+///////////////////////// End Main Functions //////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -42,45 +107,96 @@ function initReservationNewSeason()
 // User clicked the create new event XML files button
 function onClickOfXmlCreateNewButton()
 {
-    var season_case = 'current';
+    var input_data = getNewSeasonDataInput();
 
-    var main_dir = 'ReservationLayout'; // Development
-    // var main_dir = 'Reservation'; // Release
+    if (!input_data.dataIsValid())
+    {
+        return;
+    }
 
-    var result_dir = 'Spagi_76_Chairs_V_2';
-
-    var sub_xml_dir = 'SaisonXML';
-
-    var xml_filename = 'EventProgram.xml';
-
-    var callback_fctn = callbackEventProgramCreated;
-
-    var create_event_xml = new  SeasonToEventProgramXml(season_case, main_dir, result_dir, sub_xml_dir, xml_filename, callback_fctn);
-
-	//TODO createNewXmlEventFiles();
+    execCreateEventProgramXmlFile();
 
 }// onClickOfXmlCreateNewButton
 
-// 
-function callbackEventProgramCreated()
+// Returns an instance of NewSeasonData
+function getNewSeasonDataInput()
 {
+    var ret_new_season_data = new NewSeasonData();
 
-} // callbackEventProgramCreated
+    ret_new_season_data.setMainDir(g_layout_main_dir_text_box.getValue());
+
+    ret_new_season_data.setResultDir(g_layout_server_dir_text_box.getValue());
+
+    ret_new_season_data.checkData();
+
+    return ret_new_season_data;
+
+} // getNewSeasonDataInput
+
+function setNewSeasonLocalStorageData()
+{
+    var new_season_data = new NewSeasonData();
+
+    new_season_data.setMainDir(g_layout_main_dir_text_box.getValue());
+
+    new_season_data.setResultDir(g_layout_server_dir_text_box.getValue());
+
+    NewSeasonStorage.setLocal(new_season_data);
+
+} // setNewSeasonLocalStorageData
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////// End Event Functions /////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////// Start Global Parameters /////////////////////////////////////////
+///////////////////////// Start Create Controls ///////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 // Creates all the controls for the application
 function createReservationNewSeasonControls()
 {
+    createTextBoxMainDirectory();
+
+    createTextBoxResultDirectory();
+
     createXmlCreateNewButton();
 
 } // createReservationNewSeasonControls
+
+// Create the text box for the organisation directory
+function createTextBoxMainDirectory()
+{
+    g_layout_main_dir_text_box = new JazzTextBox("id_season_main_dir", 'id_div_season_main_dir');
+
+    g_layout_main_dir_text_box.setLabelText("Reservation oder ReservationLayout");
+
+    g_layout_main_dir_text_box.setLabelTextPositionAbove();
+
+    g_layout_main_dir_text_box.setSize("30");
+
+    g_layout_main_dir_text_box.setReadOnlyFlag(false);
+
+    g_layout_main_dir_text_box.setTitle("Für Release Ordner Reservation eingeben");
+
+} // createTextBoxMainDirectory
+
+// Create the text box for the result server directory where generated files shall be stored
+function createTextBoxResultDirectory()
+{
+    g_layout_server_dir_text_box = new JazzTextBox("id_season_result_dir", 'id_div_season_result_dir');
+
+    g_layout_server_dir_text_box.setLabelText("Ordner für Konzertsaal");
+
+    g_layout_server_dir_text_box.setLabelTextPositionAbove();
+
+    g_layout_server_dir_text_box.setSize("30");
+
+    g_layout_server_dir_text_box.setReadOnlyFlag(false);
+
+    g_layout_server_dir_text_box.setTitle("Name des Server Ordners für den neuen Konzertsaal.");
+
+} // createTextBoxResultDirectory
 
 // Creates the XML create new button
 function createXmlCreateNewButton()
